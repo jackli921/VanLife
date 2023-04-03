19593baa30b37b0375397920b9d8429ef7169d46
Jack <li.jack0707@gmail.com>
2023-04-03 08:36:21 -0400

    feat: add useLocation & state to retain search query when returning to previous pagee

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,30 +1,40 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { useParams, Link, useLocation } from "react-router-dom"
 
 export default function VanDetail() {
     const params = useParams()
+    const location = useLocation()
     const [van, setVan] = React.useState(null)
 
+    console.table(location)
     React.useEffect(() => {
         fetch(`/api/vans/${params.id}`)
             .then(res => res.json())
             .then(data => setVan(data.vans))
     }, [params.id])
 
+    const search = location.state?.search || "" 
+
     return (
-        <div className="van-detail-container">
-            {van ? (
-                <div className="van-detail">
-                    <img src={van.imageUrl} />
-                    <i className={`van-type ${van.type} selected`}>
-                        {van.type}
-                    </i>
-                    <h2>{van.name}</h2>
-                    <p className="van-price"><span>${van.price}</span>/day</p>
-                    <p>{van.description}</p>
-                    <button className="link-button">Rent this van</button>
-                </div>
-            ) : <h2>Loading...</h2>}
-        </div>
-    )
+      <div className="van-detail-container">
+        <Link to={`..${search}`} relative="path" className="back-button">
+          &larr; <span>Back to all vans</span>
+        </Link>
+
+        {van ? (
+          <div className="van-detail">
+            <img src={van.imageUrl} />
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <h2>{van.name}</h2>
+            <p className="van-price">
+              <span>${van.price}</span>/day
+            </p>
+            <p>{van.description}</p>
+            <button className="link-button">Rent this van</button>
+          </div>
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </div>
+    );
 }
