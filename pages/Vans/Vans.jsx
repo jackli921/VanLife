2ed77ea4d8395f3485bd92afca887aa711661038
Jack <li.jack0707@gmail.com>
2023-04-03 10:16:35 -0400

    refactor: added dedicated function to call api

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,18 +1,21 @@
 import React from "react"
 import { Link, useSearchParams } from "react-router-dom"
+import { getVans } from '../../api'
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
     const [searchParams, setSearchParams] = useSearchParams()
     
     const typeFilter = searchParams.get("type")
+
+
     
-    console.log(searchParams.toString())
-  
     React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
+        async function loadVans(){
+          const data = await getVans()
+          setVans(data)
+        }
+        loadVans()
     }, [])
 
     const displayVans = typeFilter 
